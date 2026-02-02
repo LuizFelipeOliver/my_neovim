04337a5a4337b62c8b806bb6f497601f29f30e72
Luiz Felipe <luiz.rya@gmail.com>
2026-02-02 20:29:38 -0300

    feat: add more config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,22 +1,10 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    lazy = true,
-    keys = {
-      { "<space>e", "<cmd>Oil --float<cr>", desc = "File explorer" },
-      { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
-    },
-    opts = {
-      float = {
-        padding = 2,
-        max_width = 100,
-        max_height = 30,
-        border = "rounded",
-      },
-      view_options = {
-        show_hidden = true,
-      },
-    },
-    dependencies = { "echasnovski/mini.nvim" },
-  }
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
 }
