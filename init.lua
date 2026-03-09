53a7e791d96b7efd7a29fda6c884cf9bf9cce14b
Luiz Felipe <155105197+LuizFelipeOliver@users.noreply.github.com>
2026-03-09 08:51:57 -0300

    number index cursor

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 999
 vim.opt.cmdheight = 1
