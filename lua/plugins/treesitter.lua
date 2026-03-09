34de830db8b23234b4bee275fa3580571d0363be
Luiz Felipe <155105197+LuizFelipeOliver@users.noreply.github.com>
2026-03-09 09:12:13 -0300

    change treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,17 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
-
-      require 'nvim-treesitter.configs'.setup {
+      require 'nvim-treesitter'.setup {
         ensure_installed = {
           "lua",
           "query",
