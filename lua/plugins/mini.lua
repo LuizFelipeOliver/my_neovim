b135da76e70b19454b0f75a519682677ca525e29
Luiz Felipe <155105197+LuizFelipeOliver@users.noreply.github.com>
2026-03-09 08:46:19 -0300

    add statusline to mini

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,15 +1,18 @@
 return {
   {
-    'echasnovski/mini.nvim',
+    "echasnovski/mini.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require('mini.icons').setup()
+      require("mini.icons").setup()
       MiniIcons.mock_nvim_web_devicons()
-      require('mini.statusline').setup({ use_icons = true })
-      require('mini.pairs').setup()
+      require("mini.statusline").setup({ use_icons = true })
+      require("mini.pairs").setup()
+      require("mini.indentscope").setup({
+        symbol = "│",
+      })
 
-      local hipatterns = require('mini.hipatterns')
+      local hipatterns = require("mini.hipatterns")
       hipatterns.setup({
         highlighters = {
           hex_color = hipatterns.gen_highlighter.hex_color(),
