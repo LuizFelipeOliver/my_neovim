31fc610d6dcf0c8db6e521b5e74a59971753d95d
Luiz Felipe <155105197+LuizFelipeOliver@users.noreply.github.com>
2026-03-09 08:47:22 -0300

    remove dependecies oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,7 +3,6 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
   config = function(_, opts)
     require("oil").setup(opts)
