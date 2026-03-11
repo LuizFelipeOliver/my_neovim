b5ab2b55010ab572dd6bdff9a0d57d49580ee970
Luiz Felipe <155105197+LuizFelipeOliver@users.noreply.github.com>
2026-03-11 14:44:45 -0300

    Create javascript.lua

---
+++ lua/config/lang/javascript.lua
@@ -0,0 +1,10 @@
+return {
+  install = {
+    "TSInstall javascript typescript tsx",
+    "MasonInstall vtsls prettier",
+  },
+  uninstall = {
+    "TSUninstall javascript typescript tsx",
+    "MasonUninstall vtsls prettier",
+  }
+}
