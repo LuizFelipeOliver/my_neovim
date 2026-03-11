da820ca115c6217c301e1e09dc1820ac1f65b081
Luiz Felipe <155105197+LuizFelipeOliver@users.noreply.github.com>
2026-03-11 14:42:52 -0300

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,38 +1,28 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require 'nvim-treesitter'.setup {
-        ensure_installed = {
-          "lua",
-          "query",
-          "markdown",
-          "markdown_inline",
-          "bash",
-          "dockerfile",
-          "http",
-          "json",
-        },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        lazy = true,
+        config = function()
+            require("nvim-treesitter").setup({
+                ensure_installed = {
+                    "lua",
+                    "query",
+                    "markdown",
+                    "markdown_inline",
+                    "bash",
+                    "dockerfile",
+                    "http",
+                    "html",
+                    "json",
+                },
+            })
 
-        highlight = {
-          enable = true,
-          disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-      }
-    end,
-  }
-
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function()
+                    pcall(vim.treesitter.start)
+                end,
+            })
+        end,
+    },
 }
